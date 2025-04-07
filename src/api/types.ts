d53322536e599adab024336b5ff88b905d0867c8
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-04-07 11:54:49 +0100

    feat: add types for Artwork

---
+++ src/api/types.ts
@@ -0,0 +1,17 @@
+export interface Artwork {
+  id: string;
+  title: string;
+  artist?: string;
+  creationDate?: string;
+  image?: {
+    imageURL?: string;
+    alt_text?: string;
+  };
+  medium?: string;
+  origin?: string;
+  styles?: Array<string>;
+  description?: string;
+  exhibition_history: string;
+  museumSource: "aic" | "rijksmuseum" | "smithsonian";
+  museumLink?: string;
+}
