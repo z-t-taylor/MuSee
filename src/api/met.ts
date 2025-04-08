908de0cf004821878614ee2841866a6a862bc20a
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-04-08 12:33:03 +0100

    feat: add the Met types and adapter

---
+++ src/api/met.ts
@@ -0,0 +1,39 @@
+import { Artwork } from "./types";
+
+export interface MetAPIBaseResponse {
+  objectIDs: number;
+}
+
+interface MetSingleArtworkResponse {
+  objectID: number;
+  isPublicDomain: boolean;
+  primaryImage: string;
+  primaryImageSmall: string;
+  title: string;
+  artistDisplayName: string;
+  artistDisplayBio: string;
+  objectDate: string;
+  artistNationality: string;
+  country?: string;
+  medium: string;
+  classification: string;
+}
+
+export const adaptMetToArtwork = (item: MetSingleArtworkResponse): Artwork => ({
+  id: item.objectID.toString(),
+  title: item.title,
+  artist: item.artistDisplayName,
+  creationDate: item.objectDate,
+  image: {
+    imageURL: item.primaryImage ?? item.primaryImageSmall,
+    alt_text: `{item.title} by {item.artistDisplayName}`,
+    thumbnail: item.primaryImageSmall,
+  },
+  medium: item.medium,
+  origin: item.country ?? item.artistNationality,
+  styles: item.classification,
+  description: item.artistDisplayBio || undefined,
+  exhibition_history: undefined,
+  museumSource: "met",
+  museumLink: `https://collectionapi.metmuseum.org/public/collection/v1/objects/{item.objectID}`,
+});
