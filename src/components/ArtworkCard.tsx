ff42f31c8663de0f761edab309e6f54299daed25
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-04-10 16:38:55 +0100

    feat: add routing to single artwork

--- src/components/ArtworkCard.tsx
+++ src/components/ArtworkCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Artwork } from "../api/types";
+import { Link } from "react-router-dom";
 
 interface ArtworkCardProps {
   artwork: Artwork;
@@ -7,13 +8,18 @@ interface ArtworkCardProps {
 export const ArtworkCard: React.FC<ArtworkCardProps> = ({ artwork }) => {
   return (
     <div className="artwork-card">
-      <img
-        src={artwork.image?.imageURL}
-        alt={artwork.image?.alt_text || artwork.title}
-        loading="lazy"
-      />
-      <h2>{artwork.title}</h2>
-      <h3>{artwork.artist}</h3>
+      <Link
+        to={`/artwork/${artwork.museumSource}/${artwork.id}`}
+        className="artwork-link"
+      >
+        <img
+          src={artwork.image?.imageURL}
+          alt={artwork.image?.alt_text || artwork.title}
+          loading="lazy"
+        />
+        <h2>{artwork.title}</h2>
+        <h3>{artwork.artist}</h3>
+      </Link>
     </div>
   );
 };
