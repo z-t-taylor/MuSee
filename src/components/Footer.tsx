4d1537df7ef80bb38d2a0e8b7c5a57e7794190fe
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-04-28 15:15:24 +0100

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="sticky top-[100vh] w-full border-t-2 bg-white text-[#195183] pr-6 py-4">
+      <div className="flex justify-end">
+        <p>
+          &copy;
+          <span className="font-libre">2025 MuSee. All rights reserved.</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
